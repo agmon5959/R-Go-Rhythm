9d73c40e7214c16c8727cbb5dc8ac6565e8ebae4
DongDong <qltkd5959@gmail.com>
2024-01-15 23:24:46 +0900

    카드2

---
+++ 백준/카드2/카드2.js
@@ -0,0 +1,55 @@
+// [실버4] https://www.acmicpc.net/problem/2164
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = require("fs")
+  // .readFileSync("./input.txt")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    this.length++;
+
+    return newNode;
+  }
+  shift() {
+    this.head = this.head.next;
+    this.length--;
+  }
+}
+
+function getResult() {
+  const linkedList = new LinkedList();
+  Array.from({ length: input[0] }, (_, idx) => linkedList.push(idx + 1));
+
+  while (linkedList.length !== 1) {
+    linkedList.shift();
+    linkedList.push(linkedList.head.value);
+    linkedList.shift();
+  }
+
+  return linkedList.head.value;
+}
+
+console.log(getResult());
