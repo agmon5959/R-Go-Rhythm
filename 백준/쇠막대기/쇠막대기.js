7adb209145ee1a1ddfeef923f69e21ac1d008cfc
DongDong <qltkd5959@gmail.com>
2024-01-19 23:23:25 +0900

    쇠막대기

---
+++ 백준/쇠막대기/쇠막대기.js
@@ -0,0 +1,27 @@
+// [실버2] https://www.acmicpc.net/problem/10799
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(path).toString().trim().split("\n");
+
+function getResult() {
+  const value = input.pop();
+  const arr = [];
+  let result = 0;
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] === "(") {
+      arr.push(value[i]);
+    } else {
+      if (value[i - 1] !== undefined && value[i - 1] === "(") {
+        // 레이저인 경우
+        arr.pop();
+        result += arr.length;
+      } else {
+        arr.pop();
+        result += 1;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(getResult());
---
+++ 백준/좋은단어/좋은단어.js
@@ -0,0 +1,11 @@
+// [실버4] https://www.acmicpc.net/problem/3986
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function getResult() {}
+
+console.log(getResult());
