60452ab76ef99779afda13d5614b5d48e53f3350
DongDong <qltkd5959@gmail.com>
2024-01-20 20:18:53 +0900

    좋은단어

--- 백준/좋은단어/좋은단어.js
+++ 백준/좋은단어/좋은단어.js
@@ -1,11 +1,40 @@
-// [실버4] https://www.acmicpc.net/problem/3986
-// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-let input = require("fs")
-  .readFileSync("./input.txt")
-  .toString()
-  .trim()
-  .split("\n");
+// [실버2] https://www.acmicpc.net/problem/3986
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-function getResult() {}
+function getGoodWords(wordArr) {
+  const resultArr = [];
+  const words = [...wordArr];
+  for (let i = 0; i < words.length; i++) {
+    if (resultArr.length === 0) {
+      resultArr.push(words[i]);
+      continue;
+    }
+    if (words[i] === resultArr[resultArr.length - 1]) {
+      resultArr.pop();
+    } else {
+      resultArr.push(words[i]);
+    }
+  }
+
+  if (resultArr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getResult() {
+  const len = input.shift();
+  let goodWords = 0;
+
+  for (let i = 0; i < len; i++) {
+    if (getGoodWords(input[i])) {
+      goodWords++;
+    }
+  }
+
+  return goodWords;
+}
 
 console.log(getResult());
